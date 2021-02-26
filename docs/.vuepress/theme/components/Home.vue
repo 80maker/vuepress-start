8224ca94bbbf0e3e23d34a124a88e39782f24a79
cmgddd <cmgddd@163.com>
2021-02-26 14:46:09 +0800

    home-文章列表

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>Home</div>
+  <div>
+    <ul>
+      <li v-for="item in $pagination.pages">
+        <router-link :to="item.path">{{item.title}}</router-link>
+      </li>
+    </ul>
+  </div>
 </template>
--- docs/.vuepress/theme/index.js
+++ docs/.vuepress/theme/index.js
@@ -1,6 +1,20 @@
 // .vuepress/theme/index.js
 module.exports = (themeConfig, ctx) => {
   return {
-     // ...
+    // ...
+    plugins: [
+      [
+        '@vuepress/blog',
+        {
+          directories: [
+            {
+              id: 'post',
+              dirname: '_post',
+              path: '/'
+            }
+          ]
+        }
+      ]
+    ]
   }
 }
