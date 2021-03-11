dff248651ba51f5b586869cdf95bd23f9a5589ea
cmgddd <cmgddd@163.com>
2021-03-11 21:02:42 +0800

    up

--- docs/.vuepress/theme/components/SideBar.vue
+++ docs/.vuepress/theme/components/SideBar.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     SideBar {{$site.title}}
+    <SubNav/>
     <NavBar/>
     <FooterBar/>
   </div>
 </template>
 <script>
 import FooterBar from './FooterBar'
+import SubNav from './SubNav'
 import NavBar from './NavBar'
 export default {
   name: 'SideBar',
   components: {
     FooterBar,
-    NavBar
+    NavBar,
+    SubNav
   }
 }
 </script>
--- docs/.vuepress/theme/index.js
+++ docs/.vuepress/theme/index.js
@@ -15,7 +15,29 @@ module.exports = (themeConfig, ctx) => {
           ],
           globalPagination: {
             lengthPerPage: 5,
-          }
+          },
+          frontmatters: [
+            {
+              id: "tag",
+              keys: ['tag', 'tags'],
+              path: '/tags/',
+              frontmatter: { title: 'Tag' },
+              pagination: {
+                lengthPerPage: 10
+              }
+            },
+            {
+              id: "category",
+              keys: ['category', 'categories'],
+              path: '/categories/',
+              frontmatter: { title: 'Category' },
+              pagination: {
+                lengthPerPage: 10,
+                prevText: '',
+                nextText: ''
+              }
+            }
+          ]
         }
       ]
     ]
