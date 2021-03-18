2a5cdefe0b3e99838fb48c5f17ec0c639df84a21
cmgddd <cmgddd@163.com>
2021-03-18 09:25:39 +0800

    add 添加footerbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,27 @@ module.exports = {
       { text: '🏠 内部链接', link: '/2021/03/02/one/' },
       { text: '🔥 外部链接(External)', link: 'https://google.com' },
     ],
+    social: [
+      {
+        type: 'email',
+        link: 'cmgddd@163.com'
+      },
+      {
+        type: 'github',
+        link: '80maker'
+      },
+      {
+        type: 'qq',
+        link: '//qm.qq.com/cgi-bin/qm/qr?k=5qf0UGmRJWavJGOgo5nemY2BezuGKa-V&jump_from=webapi'
+      },
+      {
+        type: 'feed',
+        link: '/rss.xml'
+      }
+    ],
     copyright: `© ${new Date().getFullYear()} ❤️ <a target="_blank" rel="external nofollow noopener" href="https://17ria.com/">Neil Chen</a>`
-  }
+  },
+  plugins: [
+    require('./theme/plugin/util')
+  ]
 }
--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -3,18 +3,21 @@
     <SideBar/>
     <Post v-if="$page.pid === 'post'"/>
     <Home v-else />
+    <SvgSprite/>
   </div>
 </template>
 <script>
 import Post from '../components/Post'
 import Home from '../components/Home'
 import SideBar from '../components/SideBar'
+import SvgSprite from '../components/SvgSprite.vue'
 export default {
   name: 'Layout',
   components: {
     Home,
     SideBar,
-    Post
+    Post,
+    SvgSprite
   }
 }
 </script>
